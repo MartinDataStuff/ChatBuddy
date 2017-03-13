abe0fbce7167213493a36e791dffbeeda786d990
MartinDataStuff <martin1-g@hotmail.com>
2017-03-13 10:56:00 +0100

    Fixed merge issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,21 +11,16 @@ import {AngularFireModule, AuthProviders, AuthMethods} from 'angularfire2';
 
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
-<<<<<<< HEAD
+
 import { HomeComponent } from './home/home.component';
 import {ChatBuddyRoutingModule} from "./chat-buddy-routing.module";
-=======
+
 export const firebaseConfig = {
   apiKey: "AIzaSyAdO4NXTo52JDsXs6YG-IdjUFlXjaRfv0M",
   authDomain: "cp2test-f105d.firebaseapp.com",
   databaseURL: "https://cp2test-f105d.firebaseio.com",
   storageBucket: "cp2test-f105d.appspot.com",
   messagingSenderId: "162855485996"
-<<<<<<< HEAD
-=======
->>>>>>> e7422341ffeb8a9a4c6ff1591337d7df26d96214
-
->>>>>>> 9e380874a81e804ba28472a40a17f8efff623111
 };
 export const firebarebaseLoginConfig =  {
   provider: AuthProviders.Password,
@@ -44,11 +39,9 @@ export const firebarebaseLoginConfig =  {
     MaterialModule,
     Angular2FontawesomeModule,
     FlexLayoutModule,
-<<<<<<< HEAD
-    ChatBuddyRoutingModule
-=======
+    ChatBuddyRoutingModule,
     AngularFireModule.initializeApp(firebaseConfig, firebarebaseLoginConfig)
->>>>>>> e7422341ffeb8a9a4c6ff1591337d7df26d96214
+
   ],
   providers: [],
   bootstrap: [AppComponent]
