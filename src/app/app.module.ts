e7422341ffeb8a9a4c6ff1591337d7df26d96214
MartinDataStuff <martin1-g@hotmail.com>
2017-03-13 10:34:05 +0100

    Added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,22 @@ import { MaterialModule } from '@angular/material';
 import 'hammerjs';
 import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import {AngularFireModule, AuthProviders, AuthMethods} from 'angularfire2';
 
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+export const firebaseConfig = {
+  apiKey: "AIzaSyAdO4NXTo52JDsXs6YG-IdjUFlXjaRfv0M",
+  authDomain: "cp2test-f105d.firebaseapp.com",
+  databaseURL: "https://cp2test-f105d.firebaseio.com",
+  storageBucket: "cp2test-f105d.appspot.com",
+  messagingSenderId: "162855485996"
 
+};
+export const firebarebaseLoginConfig =  {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Password
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +35,8 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
     HttpModule,
     MaterialModule,
     Angular2FontawesomeModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    AngularFireModule.initializeApp(firebaseConfig, firebarebaseLoginConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
