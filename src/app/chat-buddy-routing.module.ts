a435500199c3b22f35b8722399a8825d27c12920
Doh09 <alexandreasen09@gmail.com>
2017-03-20 13:06:35 +0100

    added chat input and send chat button

--- src/app/chat-buddy-routing.module.ts
+++ src/app/chat-buddy-routing.module.ts
@@ -5,12 +5,14 @@ import {LobbyComponent} from "./lobby/lobby/lobby.component";
 import {ContactComponent} from "./contact/contact.component";
 import {AboutComponent} from "./about/about.component";
 import {LoginComponent} from "./facebook-auth/login/login.component";
+import {ChatroomComponent} from "./chat-room/chatroom/chatroom.component";
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {  path: 'home', component: HomeComponent },
 {  path: 'login', component: LoginComponent },
   {  path: 'lobby', component: LobbyComponent },
   {  path: 'contact', component: ContactComponent },
+  {  path: 'testchatroom', component: ChatroomComponent },
 {  path: 'about', component: AboutComponent }];
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
--- src/app/chat-room/chatroom/chatroom.component.ts
+++ src/app/chat-room/chatroom/chatroom.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatroomComponent implements OnInit {
 
-  constructor() { }
+  dotCounter: number;
+  isTyping: boolean;
+  chatOutput: string;
+  chatInput:string;
+  constructor() {
+    this.dotCounter = 1;
+  }
+
+  get getDotCounter(){
+    return this.dotCounter;
+  }
 
   ngOnInit() {
+    this.startDotCounter(1000);
+  }
+
+  enterText(){
+  this.chatOutput = this.chatInput;
+  this.chatInput = "";
+    console.log("dotCounter: "+this.dotCounter);
+  }
+
+  startDotCounter(interval: number){
+
+    window.setInterval(function(){
+      if (!!this.dotCounter === !!NaN && this.dotCounter !== 0)
+      {
+        console.log("It's not a number")
+       this.dotCounter = 0;
+      }
+      this.dotCounter++;
+      this.dotCounter = this.dotCounter % 3; //number after % represents max amount of dots
+      console.log("dotCounter: "+this.dotCounter);
+    }, interval);
   }
 
 }
