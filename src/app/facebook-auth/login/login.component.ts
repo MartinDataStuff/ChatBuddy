57495ccf998b057171ee3686c287db74df4a6a63
MartinDataStuff <martin1-g@hotmail.com>
2017-03-22 15:27:11 +0100

    Made so Login still responds

--- src/app/facebook-auth/login/login.component.ts
+++ src/app/facebook-auth/login/login.component.ts
@@ -9,12 +9,12 @@ import {FacebookService, FacebookInitParams} from 'ng2-facebook-sdk';
 export class LoginComponent implements OnInit {
 
   constructor(private fb: FacebookService) {
-/*    let fbParams: FacebookInitParams = {
+   let fbParams: FacebookInitParams = {
       appId: '662555623935980',
       xfbml: true,
       version: 'v2.8'
     };
-    this.fb.init(fbParams);*/
+    /*this.fb.init(fbParams);*/
   }
   ngOnInit() {
   }
